394ca7b4df66c3b5f314847a61eba614417513fe
DingYi <joeklaus@126.com>
2017-01-11 03:36:47 +0800

    业务领域展现形式

--- src/page/home/tpl/practice.vue
+++ src/page/home/tpl/practice.vue
@@ -3,12 +3,14 @@
 		<h2>业务领域</h2>
 		<div v-for="(practiceType, index) in practiceList" v-if="practiceType.list.length != 0">
 			<h3>{{practiceList[index].name}}</h3>
-			<ul class="practice z-clearfix">
-				<li v-for="practice in practiceType.list">
-					<div class="name">{{practice.title}}</div>
-				</li>
-			</ul>
-			<div></div>
+			<div v-for="(groupList, groupListIndex) in practiceType.groupList">
+				<ul class="practice z-clearfix">
+					<li v-for="group in groupList" @click="showPracticeDetail(groupList.id, group.id)">
+						<div class="name">{{group.title}}</div>
+					</li>
+				</ul>
+				<div class="practiceDetail" :class="groupList.id == groudListID ? 'show-detail' : ''" v-html="content"></div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -21,9 +23,17 @@
 				practiceList: [],
 				lang: 1,
 				groupNum: 4,
+				practiceID: 0,
+				groudListID: 0,
+				content: "",
 			}
 		},
 		mounted: function () {
+			if(navigator.userAgent.match(/(phone|pod|iPhone|iPod|ios|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)){
+				this.groupNum = 2
+			} else {
+				this.groupNum = 4
+			}
 			if(localStorage.getItem("lang") != null) {
 				this.lang = localStorage.getItem("lang")
 			}
@@ -58,22 +68,21 @@
     									if(j == 0 || (j != 0 && j%_self.groupNum !=0)) {
     										group.push(dataGroupList[j])
     									} else {
+    										group.id = (i+1)*10 + (j+1)
     										groupList.push(group)
     										group = []
     										group.push(dataGroupList[j])
     									}
     									if(j == dataGroupList.length-1) {
+    										group.id = (i+1)*10 + (j+1)
     										groupList.push(group)
     									}
     								}
-    								console.log(dataList[i])
-    								console.log(groupList)
-    								dataList[i].push('groupList', groupList)
+    								dataList[i].groupList = groupList
     							}
     						}
     					}
     					_self.practiceList = dataList
-    					console.log(_self.practiceList)
     				} else {
     					alert('status: ' + status)
     				}
@@ -81,6 +90,29 @@
     				// TODO 错误toast提示
   				})
 			},
+			showPracticeDetail: function(groudListID, practiceID) {
+				if(this.practiceID != practiceID) {
+    				this.content = ""
+					this.groudListID = groudListID
+					this.practiceID = practiceID
+					this.loadPracticeDetail()
+				}
+			},
+			loadPracticeDetail: function() {
+				const _self = this
+				this.$http.get('http://www.lutong.com/api/index.php?c=practices&m=get_practices_info&lang=' + _self.lang + '&id=' + _self.practiceID,
+				).then((response) => {
+					const data = response.data
+					const status = response.data.status
+    				if(status === 1) {
+    					_self.content = data.info.content
+    				} else {
+    					alert('status: ' + status)
+    				}
+  				}, (response) => {
+    				// TODO 错误toast提示
+  				})
+			}
 		}
 	}
 </script>
@@ -100,15 +132,15 @@
 		}
 
 		ul {
-			margin-bottom: 1.66666667rem;
+			margin-top: 1.66666667rem;
 			padding: 0 2.5rem;
 
 			li {
 				float: left;
 				width: 25%;
-				margin-bottom: 1.66666667rem;
 				padding: 0 0.83333333rem;
 				height: 8.33333333rem;
+				cursor: pointer;
 
 				.name {
 					display: block;
@@ -121,11 +153,24 @@
 				}
 			}
 		}
+
+		.practiceDetail {
+			display: none;
+			margin-top: 1.66666667rem;
+			margin-bottom: 1.66666667rem;
+			padding: 3.33333333rem 8.33333333rem;
+			border-top: solid 0.08333333rem #e6e6e6;
+			border-bottom: solid 0.08333333rem #e6e6e6;
+		}
+
+		.show-detail {
+			display: block;
+		}
 	}
 
 @media only screen and (max-width: 480px) {
 	.practice {
-		padding: 1.66666667rem 1.25rem;
+		padding: 1.66666667rem 0;
 
 		h2 {
 			margin-bottom: 0.83333333rem;
@@ -147,7 +192,6 @@
 				float: left;
 				width: 50%;
 				height: auto;
-				margin-bottom: 1.66666667rem;
 				padding: 0 1.25rem;
 
 				.name {
